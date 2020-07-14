063eba0a6be7246ab729f153b5c32556696440f6
rahulbhati2304 <66083004+rahulbhati2304@users.noreply.github.com>
2020-07-14 18:57:31 +0530

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 
-const Card = () => {
+const Card = ({ name, email, id }) => {
   return (
-    <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
-      <img alt="robots" src={`https://robohash.org/test?size=200x200`} />
+    <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
       <div>
-        <h2>Jane Doe</h2>
-        <p>jane.doe@gmail.com</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
-};
+}
 
 export default Card;
