3d851d37a3bc93a85d73d8e621c91b14dda2a8a8
rahulbhati2304 <66083004+rahulbhati2304@users.noreply.github.com>
2020-07-14 17:36:34 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import Card from "./Card";
+
+ReactDOM.render(
+  <div>
+    <Card />
+    <Card />
+    <Card />
+  </div>,
+  document.getElementById("root")
+);
