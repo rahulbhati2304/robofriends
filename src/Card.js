802bba45555835853fa86c18ae01b23d3c4f1285
rahulbhati2304 <66083004+rahulbhati2304@users.noreply.github.com>
2020-07-14 18:53:39 +0530

    fdsf

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = () => {
   return (
     <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
-      <img alt="robots" src=`https://robohash.org/test?size=200x200` />
+      <img alt="robots" src={`https://robohash.org/test?size=200x200`} />
       <div>
         <h2>Jane Doe</h2>
         <p>jane.doe@gmail.com</p>
